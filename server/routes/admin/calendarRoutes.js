240666c3aff84ddac201f348b5861ef81c648a74
Guilherme Bandeira <107276182+DevGuiBan@users.noreply.github.com>
2025-06-01 15:05:04 -0300

    fixing route delete calendar

--- server/routes/admin/calendarRoutes.js
+++ server/routes/admin/calendarRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import autenticarToken from '../../middlewares/authMiddleware.js';
 import isAdmin from '../../middlewares/isAdmin.js';
-import { createCalendar, updateCalendar, getCalendarTypes, listCalendars } from '../../controllers/admin/calendarController.js';
+import { createCalendar, updateCalendar, getCalendarTypes, listCalendars, deleteCalendar } from '../../controllers/admin/calendarController.js';
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post('/calendar', autenticarToken, isAdmin, createCalendar);
 router.get('/calendar-types', autenticarToken, getCalendarTypes);
 router.put('/calendar/:id', autenticarToken, isAdmin, updateCalendar);
 router.get('/calendar', autenticarToken, isAdmin, listCalendars);
+router.delete('/calendar/:id', autenticarToken, isAdmin, deleteCalendar);
 
 
 export default router;

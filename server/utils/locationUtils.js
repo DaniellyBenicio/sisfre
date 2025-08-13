398e90bca9e44a98177e67179d2398d2df2a3996
Guilherme Bandeira <107276182+DevGuiBan@users.noreply.github.com>
2025-08-13 10:27:46 -0300

    alterando a localização

--- server/utils/locationUtils.js
+++ server/utils/locationUtils.js
@@ -1,9 +1,10 @@
 import haversine from "haversine-distance";
 
-const CAMPUS_COORDS = { latitude: -6.600767156023205, longitude: -39.05512157768739 };
+const CAMPUS_COORDS = { latitude: -6.353131736454447, longitude: -39.2963145500635 };
 const MAX_DIST = 100; // metros
 
--6.600767156023205, -39.05512157768739
+// Campus -6.600767156023205, -39.05512157768739
+// my house -6.353131736454447, -39.2963145500635
 
 export const isInCampus = (coords) => {
   const distance = haversine(CAMPUS_COORDS, coords);
